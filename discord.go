3eccee53e0589b85a60376fa0bac6cf0e7d1300e
Arion Silver <arionsilver@gmail.com>
2019-11-24 22:36:11 +0900

    gofmt

--- discord.go
+++ discord.go
@@ -1,93 +1,93 @@
 package main
 
 import (
-    "encoding/json"
-    "fmt"
-    "io/ioutil"
-    "log"
-    "net/http"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
 
-    "github.com/arionsilver/twitch_stream_supporter/twitch"
-    "github.com/bwmarrin/discordgo"
+	"github.com/arionsilver/twitch_stream_supporter/twitch"
+	"github.com/bwmarrin/discordgo"
 )
 
 func startDiscordBot(auth string, config twitch.Config, q chan bool) (c chan bool) {
-    c = make(chan bool)
-    session, err := discordgo.New(auth)
-    if err != nil {
-    	log.Printf("Error while starting discord session: %s", err)
-    	go func() { c <- true }()
-    	return
-    }
-    go executeDiscordBot(session, config, c, q)
-    go createSimpleHTTPServer(session, config)
+	c = make(chan bool)
+	session, err := discordgo.New(auth)
+	if err != nil {
+		log.Printf("Error while starting discord session: %s", err)
+		go func() { c <- true }()
+		return
+	}
+	go executeDiscordBot(session, config, c, q)
+	go createSimpleHTTPServer(session, config)
 
-    return
+	return
 }
 
 func executeDiscordBot(session *discordgo.Session, config twitch.Config, c chan bool, q chan bool) {
-    defer func() { c <- true }()
+	defer func() { c <- true }()
 
-    if err := session.Open(); err != nil {
-    	log.Printf("Couldn't open discord session: %s", err)
-    	return
-    }
+	if err := session.Open(); err != nil {
+		log.Printf("Couldn't open discord session: %s", err)
+		return
+	}
 
-    defer session.Close()
-    session.AddHandler(func(s *discordgo.Session, event *discordgo.GuildCreate) {
-    	for _, channel := range event.Guild.Channels {
-    		fmt.Printf("Channel ID= %s GuildID= %s Name= %s\n", channel.ID, channel.GuildID, channel.Name)
-    	}
-    })
+	defer session.Close()
+	session.AddHandler(func(s *discordgo.Session, event *discordgo.GuildCreate) {
+		for _, channel := range event.Guild.Channels {
+			fmt.Printf("Channel ID= %s GuildID= %s Name= %s\n", channel.ID, channel.GuildID, channel.Name)
+		}
+	})
 
-    <-q // wait on quit
+	<-q // wait on quit
 }
 
 type rootHandler struct {
-    session *discordgo.Session
-    config  twitch.Config
+	session *discordgo.Session
+	config  twitch.Config
 }
 
 func (handler rootHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-    defer w.WriteHeader(200)
-    r.ParseForm()
-    if len(r.Form.Get("hub.challenge")) > 0 {
-    	w.Write([]byte(r.Form.Get("hub.challenge")))
-    } else if r.ContentLength > 0 {
-    	body, err := ioutil.ReadAll(r.Body)
-    	if err != nil {
-    		log.Printf("Failed while reading request body: %s", err)
-    		return
-    	}
+	defer w.WriteHeader(200)
+	r.ParseForm()
+	if len(r.Form.Get("hub.challenge")) > 0 {
+		w.Write([]byte(r.Form.Get("hub.challenge")))
+	} else if r.ContentLength > 0 {
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			log.Printf("Failed while reading request body: %s", err)
+			return
+		}
 
-    	type jsonRequest struct {
-    		Data []struct {
-    			ID	string `json:"id"`
-    			Title    string `json:"title"`
-    			UserName string `json:"user_name"`
-    		} `json:"data"`
-    	}
+		type jsonRequest struct {
+			Data []struct {
+				ID       string `json:"id"`
+				Title    string `json:"title"`
+				UserName string `json:"user_name"`
+			} `json:"data"`
+		}
 
-    	log.Printf("Body: %s", body)
+		log.Printf("Body: %s", body)
 
-    	var info jsonRequest
-    	if err = json.Unmarshal(body, &info); err != nil {
-    		log.Printf("Couldn't parse the request as a JSON: %s", err)
-    		return
-    	}
+		var info jsonRequest
+		if err = json.Unmarshal(body, &info); err != nil {
+			log.Printf("Couldn't parse the request as a JSON: %s", err)
+			return
+		}
 
-    	for _, channel := range handler.config.Channels {
-    		for _, data := range info.Data {
-    			content := fmt.Sprintf("%s has started streaming.\nStream title: **%s**\nhttp://twitch.tv/%s", data.UserName, data.Title, data.UserName)
-    			handler.session.ChannelMessageSend(channel, content)
-    		}
-    	}
-    } else {
-    	log.Printf("Empty request received.\n\tHeaders: %+v\n\tRemote address: %s", r.Header, r.RemoteAddr)
-    }
+		for _, channel := range handler.config.Channels {
+			for _, data := range info.Data {
+				content := fmt.Sprintf("%s has started streaming.\nStream title: **%s**\nhttp://twitch.tv/%s", data.UserName, data.Title, data.UserName)
+				handler.session.ChannelMessageSend(channel, content)
+			}
+		}
+	} else {
+		log.Printf("Empty request received.\n\tHeaders: %+v\n\tRemote address: %s", r.Header, r.RemoteAddr)
+	}
 }
 
 func createSimpleHTTPServer(session *discordgo.Session, config twitch.Config) {
-    http.Handle("/", rootHandler{session, config})
-    log.Fatal(http.ListenAndServe(config.Port, nil))
+	http.Handle("/", rootHandler{session, config})
+	log.Fatal(http.ListenAndServe(config.Port, nil))
 }
--- twitch/endpoints.go
+++ twitch/endpoints.go
@@ -1,10 +1,10 @@
 package twitch
 
 import (
-    "bytes"
-    "log"
-    "net/http"
-    "net/url"
+	"bytes"
+	"log"
+	"net/http"
+	"net/url"
 )
 
 const baseURL string = "https://api.twitch.tv/helix"
@@ -18,76 +18,76 @@ const generateTokenEndpoint string = "/token"
 const validateTokenEndpoint string = "/validate"
 
 func getUsers(login string) (result *http.Request, err error) {
-    result, err = http.NewRequest("GET", baseURL+usersEndpoint, nil)
-    if err != nil {
-    	log.Printf("Error creating getUsers request: %s", err)
-    	return
-    }
+	result, err = http.NewRequest("GET", baseURL+usersEndpoint, nil)
+	if err != nil {
+		log.Printf("Error creating getUsers request: %s", err)
+		return
+	}
 
-    query := url.Values{}
-    query.Add("login", login)
-    result.URL.RawQuery = query.Encode()
+	query := url.Values{}
+	query.Add("login", login)
+	result.URL.RawQuery = query.Encode()
 
-    return
+	return
 }
 
 func getWebhooksSubscriptions(page *string) (result *http.Request, err error) {
-    result, err = http.NewRequest("GET", baseURL+webhooksSubscriptionsEndpoint, nil)
-    if err != nil {
-    	log.Printf("Error creating getWebhooksSubscriptions request: %s", err)
-    	return
-    }
+	result, err = http.NewRequest("GET", baseURL+webhooksSubscriptionsEndpoint, nil)
+	if err != nil {
+		log.Printf("Error creating getWebhooksSubscriptions request: %s", err)
+		return
+	}
 
-    query := url.Values{}
-    query.Add("first", "100")
-    if page != nil {
-    	query.Add("after", *page)
-    }
-    result.URL.RawQuery = query.Encode()
+	query := url.Values{}
+	query.Add("first", "100")
+	if page != nil {
+		query.Add("after", *page)
+	}
+	result.URL.RawQuery = query.Encode()
 
-    return
+	return
 }
 
 func postWebhookSubscription(body []byte) (result *http.Request, err error) {
-    result, err = http.NewRequest("POST", baseURL+webhooksHubEndpoint, bytes.NewReader(body))
-    if err != nil {
-    	log.Printf("Error creating postWebhookSubscription request: %s", err)
-    }
+	result, err = http.NewRequest("POST", baseURL+webhooksHubEndpoint, bytes.NewReader(body))
+	if err != nil {
+		log.Printf("Error creating postWebhookSubscription request: %s", err)
+	}
 
-    return
+	return
 }
 
 func subscribeStreamTopic(userID string) string {
-    query := url.Values{}
-    query.Add("user_id", userID)
-    return baseURL + streamsEndpoint + "?" + query.Encode()
+	query := url.Values{}
+	query.Add("user_id", userID)
+	return baseURL + streamsEndpoint + "?" + query.Encode()
 }
 
 func postAppToken(clientID, clientSecret string) (result *http.Request, err error) {
-    result, err = http.NewRequest("POST", baseOAuthURL+generateTokenEndpoint, nil)
-    if err != nil {
-    	log.Printf("Error creating postAppToken request: %s", err)
-    	return
-    }
+	result, err = http.NewRequest("POST", baseOAuthURL+generateTokenEndpoint, nil)
+	if err != nil {
+		log.Printf("Error creating postAppToken request: %s", err)
+		return
+	}
 
-    query := url.Values{}
-    query.Add("client_id", clientID)
-    query.Add("client_secret", clientSecret)
-    query.Add("grant_type", "client_credentials")
-    result.URL.RawQuery = query.Encode()
+	query := url.Values{}
+	query.Add("client_id", clientID)
+	query.Add("client_secret", clientSecret)
+	query.Add("grant_type", "client_credentials")
+	result.URL.RawQuery = query.Encode()
 
-    return
+	return
 }
 
 func validateAppToken(token string) (result *http.Request, err error) {
-    result, err = http.NewRequest("GET", baseOAuthURL+validateTokenEndpoint, nil)
-    if err != nil {
-    	log.Printf("Error creating validateAppToken request: %s", err)
-    	return
-    }
+	result, err = http.NewRequest("GET", baseOAuthURL+validateTokenEndpoint, nil)
+	if err != nil {
+		log.Printf("Error creating validateAppToken request: %s", err)
+		return
+	}
 
-    result.Header = http.Header{}
-    result.Header.Add("Authorization", "OAuth "+token)
+	result.Header = http.Header{}
+	result.Header.Add("Authorization", "OAuth "+token)
 
-    return
+	return
 }
