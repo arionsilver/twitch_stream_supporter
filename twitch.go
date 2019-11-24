b9c5e7cbf1693d708ee24cc67ae31e8385bad067
Arion Silver <arionsilver@gmail.com>
2019-11-24 22:34:34 +0900

    golint

--- twitch.go
+++ twitch.go
@@ -1,96 +1,96 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"log"
-	"os"
-	"strconv"
-	"strings"
+    "bufio"
+    "fmt"
+    "log"
+    "os"
+    "strconv"
+    "strings"
 
-	"github.com/arionsilver/twitch_stream_supporter/twitch"
+    "github.com/arionsilver/twitch_stream_supporter/twitch"
 )
 
 func startTwitchHelper(auth AuthInfo, tokenFile string, config twitch.Config, q chan bool) (c chan bool) {
-	c = make(chan bool)
-	go executeTwitchHelper(auth, tokenFile, config, c, q)
+    c = make(chan bool)
+    go executeTwitchHelper(auth, tokenFile, config, c, q)
 
-	return
+    return
 }
 
 func executeTwitchHelper(auth AuthInfo, tokenFile string, config twitch.Config, c chan bool, q chan bool) {
-	defer func() { c <- true }()
+    defer func() { c <- true }()
 
-	session := twitch.NewSession(auth.Twitch, auth.TwitchClientID, auth.TwitchClientSecret, config)
-	validToken, err := session.CheckToken(tokenFile)
-	if err != nil {
-		log.Printf("Error while validating twitch token. Quitting...\n%s", err)
-		return
-	}
+    session := twitch.NewSession(auth.Twitch, auth.TwitchClientID, auth.TwitchClientSecret, config)
+    validToken, err := session.CheckToken(tokenFile)
+    if err != nil {
+    	log.Printf("Error while validating twitch token. Quitting...\n%s", err)
+    	return
+    }
 
-	if !validToken {
-		err = session.GenerateToken()
-		if err != nil {
-			log.Printf("Error while generating new twitch token. Quitting...\n%s", err)
-			return
-		}
-	}
+    if !validToken {
+    	err = session.GenerateToken()
+    	if err != nil {
+    		log.Printf("Error while generating new twitch token. Quitting...\n%s", err)
+    		return
+    	}
+    }
 
-	go waitOnInput(session)
-	<-q // wait on quit
+    go waitOnInput(session)
+    <-q // wait on quit
 }
 
 func waitOnInput(session twitch.Session) {
-	stdin := bufio.NewScanner(os.Stdin)
-	for stdin.Scan() {
-		if err := stdin.Err(); err != nil {
-			log.Printf("Error occurred reading stdin!?: %s", err)
-		}
+    stdin := bufio.NewScanner(os.Stdin)
+    for stdin.Scan() {
+    	if err := stdin.Err(); err != nil {
+    		log.Printf("Error occurred reading stdin!?: %s", err)
+    	}
 
-		text := stdin.Text()
-		split := strings.Split(text, " ")
-		if len(split) > 0 {
-			executeCommand(session, split)
-		}
-	}
+    	text := stdin.Text()
+    	split := strings.Split(text, " ")
+    	if len(split) > 0 {
+    		executeCommand(session, split)
+    	}
+    }
 }
 
 func executeCommand(session twitch.Session, args []string) {
-	switch args[0] {
-	case "user":
-		if len(args) > 1 {
-			userName := args[1]
-			user, err := session.GetUserInfo(userName)
-			if err != nil {
-				log.Printf("Error while fetch user info: %s", err)
-				return
-			}
+    switch args[0] {
+    case "user":
+    	if len(args) > 1 {
+    		userName := args[1]
+    		user, err := session.GetUserInfo(userName)
+    		if err != nil {
+    			log.Printf("Error while fetch user info: %s", err)
+    			return
+    		}
 
-			fmt.Printf("User info:\n\tID: %s\n\tDisplay Name: %s\n", user.ID, user.DisplayName)
-		}
-	case "webhooks":
-		subs, err := session.GetWebhooks()
-		if err != nil {
-			log.Printf("Error while fetching webhooks: %s", err)
-			return
-		}
+    		fmt.Printf("User info:\n\tID: %s\n\tDisplay Name: %s\n", user.ID, user.DisplayName)
+    	}
+    case "webhooks":
+    	subs, err := session.GetWebhooks()
+    	if err != nil {
+    		log.Printf("Error while fetching webhooks: %s", err)
+    		return
+    	}
 
-		for _, sub := range subs {
-			fmt.Printf("Subscription Information: %+v\n", sub)
-		}
-	case "subscribe":
-		if len(args) > 2 {
-			id := args[1]
-			var lease int
-			if args[2] == "max" {
-				lease = 864000
-			} else {
-				lease, _ = strconv.Atoi(args[2])
-			}
+    	for _, sub := range subs {
+    		fmt.Printf("Subscription Information: %+v\n", sub)
+    	}
+    case "subscribe":
+    	if len(args) > 2 {
+    		id := args[1]
+    		var lease int
+    		if args[2] == "max" {
+    			lease = 864000
+    		} else {
+    			lease, _ = strconv.Atoi(args[2])
+    		}
 
-			if err := session.SubscribeStream(id, lease); err != nil {
-				log.Printf("Stream subscription failed: %s", err)
-			}
-		}
-	}
+    		if err := session.SubscribeStream(id, lease); err != nil {
+    			log.Printf("Stream subscription failed: %s", err)
+    		}
+    	}
+    }
 }
--- twitch/twitch.go
+++ twitch/twitch.go
@@ -1,201 +1,201 @@
 package twitch
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
-	"time"
+    "encoding/json"
+    "fmt"
+    "io/ioutil"
+    "log"
+    "net/http"
+    "time"
 )
 
 // Config config
 type Config struct {
-	CallbackServer string   `json:"callback_server"`
-	Channels       []string `json:"channels"`
-	Port           string   `json:"port"`
+    CallbackServer string   `json:"callback_server"`
+    Channels       []string `json:"channels"`
+    Port           string   `json:"port"`
 }
 
 // Session session
 type Session struct {
-	authToken    string
-	clientID     string
-	clientSecret string
-	client       http.Client
-	config       Config
+    authToken    string
+    clientID     string
+    clientSecret string
+    client       http.Client
+    config       Config
 
-	tokenFile string
-	token     tokenInfo
+    tokenFile string
+    token     tokenInfo
 }
 
 // UserInfo user info
 type UserInfo struct {
-	ID          string `json:"id"`
-	DisplayName string `json:"display_name"`
+    ID          string `json:"id"`
+    DisplayName string `json:"display_name"`
 }
 
 // SubscriptionInfo subscription info
 type SubscriptionInfo struct {
-	Topic    string    `json:"topic"`
-	Callback string    `json:"callback"`
-	Expires  time.Time `json:"expires_at"`
+    Topic    string    `json:"topic"`
+    Callback string    `json:"callback"`
+    Expires  time.Time `json:"expires_at"`
 }
 
 type dataResult struct {
-	Total      int               `json:"total"`
-	Data       []json.RawMessage `json:"data"`
-	Pagination struct {
-		Cursor string `json:"cursor"`
-	} `json:"pagination"`
+    Total      int               `json:"total"`
+    Data       []json.RawMessage `json:"data"`
+    Pagination struct {
+    	Cursor string `json:"cursor"`
+    } `json:"pagination"`
 }
 
 // NewSession creates session
 func NewSession(auth, clientID, clientSecret string, config Config) (result Session) {
-	result.authToken = auth
-	result.clientID = clientID
-	result.clientSecret = clientSecret
-	result.client.Timeout = 10 * time.Second
-	result.config = config
+    result.authToken = auth
+    result.clientID = clientID
+    result.clientSecret = clientSecret
+    result.client.Timeout = 10 * time.Second
+    result.config = config
 
-	return
+    return
 }
 
 func (session Session) addBearerAuth(req *http.Request) {
-	req.Header = http.Header{}
-	req.Header.Add("Authorization", "Bearer "+session.authToken)
+    req.Header = http.Header{}
+    req.Header.Add("Authorization", "Bearer "+session.authToken)
 }
 
 func (session Session) addTokenAuth(req *http.Request) {
-	req.Header = http.Header{}
-	req.Header.Add("Authorization", "Bearer "+session.token.Token)
+    req.Header = http.Header{}
+    req.Header.Add("Authorization", "Bearer "+session.token.Token)
 }
 
 // GetUserInfo returns user info
 func (session Session) GetUserInfo(userName string) (result UserInfo, err error) {
-	req, err := getUsers(userName)
-	if err != nil {
-		return
-	}
+    req, err := getUsers(userName)
+    if err != nil {
+    	return
+    }
 
-	session.addBearerAuth(req)
-	res, err := session.client.Do(req)
-	if err != nil {
-		log.Printf("Error while execute GetUserInfo request: %s", err)
-	}
+    session.addBearerAuth(req)
+    res, err := session.client.Do(req)
+    if err != nil {
+    	log.Printf("Error while execute GetUserInfo request: %s", err)
+    }
 
-	if res.StatusCode != 200 {
-		err = fmt.Errorf("Request returned: %d", res.StatusCode)
-		return
-	}
+    if res.StatusCode != 200 {
+    	err = fmt.Errorf("Request returned: %d", res.StatusCode)
+    	return
+    }
 
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		log.Printf("Error reading response body: %s", err)
-		return
-	}
+    body, err := ioutil.ReadAll(res.Body)
+    if err != nil {
+    	log.Printf("Error reading response body: %s", err)
+    	return
+    }
 
-	var d dataResult
-	if err = json.Unmarshal(body, &d); err != nil {
-		log.Printf("Error while parsing response body: %s", err)
-		log.Printf("Body: %s", body)
-		return
-	}
+    var d dataResult
+    if err = json.Unmarshal(body, &d); err != nil {
+    	log.Printf("Error while parsing response body: %s", err)
+    	log.Printf("Body: %s", body)
+    	return
+    }
 
-	for _, inner := range d.Data {
-		// return first user, even if there are multiple results.
-		err = json.Unmarshal(inner, &result)
-		return
-	}
+    for _, inner := range d.Data {
+    	// return first user, even if there are multiple results.
+    	err = json.Unmarshal(inner, &result)
+    	return
+    }
 
-	err = fmt.Errorf("User not found")
-	return
+    err = fmt.Errorf("User not found")
+    return
 }
 
 // GetWebhooks get all webhooks subscriptions
 func (session Session) GetWebhooks() (result []SubscriptionInfo, err error) {
-	req, err := getWebhooksSubscriptions(nil)
-	if err != nil {
-		return
-	}
+    req, err := getWebhooksSubscriptions(nil)
+    if err != nil {
+    	return
+    }
 
-	session.addTokenAuth(req)
-	res, err := session.client.Do(req)
-	if err != nil {
-		log.Printf("Error while executing GetWebhooks request: %s", err)
-		return
-	}
+    session.addTokenAuth(req)
+    res, err := session.client.Do(req)
+    if err != nil {
+    	log.Printf("Error while executing GetWebhooks request: %s", err)
+    	return
+    }
 
-	if res.StatusCode != 200 {
-		err = fmt.Errorf("Request returned: %d", res.StatusCode)
-		return
-	}
+    if res.StatusCode != 200 {
+    	err = fmt.Errorf("Request returned: %d", res.StatusCode)
+    	return
+    }
 
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		log.Printf("Error reading response body: %s", err)
-		return
-	}
+    body, err := ioutil.ReadAll(res.Body)
+    if err != nil {
+    	log.Printf("Error reading response body: %s", err)
+    	return
+    }
 
-	var d dataResult
-	if err = json.Unmarshal(body, &d); err != nil {
-		log.Printf("Error while parsing response body: %s", err)
-		log.Printf("Body: %s", body)
-		return
-	}
+    var d dataResult
+    if err = json.Unmarshal(body, &d); err != nil {
+    	log.Printf("Error while parsing response body: %s", err)
+    	log.Printf("Body: %s", body)
+    	return
+    }
 
-	for _, inner := range d.Data {
-		var sub SubscriptionInfo
-		if err = json.Unmarshal(inner, &sub); err != nil {
-			log.Printf("Error while parsing internal JSON: %s", err)
-			log.Printf("Invalid JSON: %s", inner)
-			err = nil
-			continue
-		}
-		result = append(result, sub)
-	}
+    for _, inner := range d.Data {
+    	var sub SubscriptionInfo
+    	if err = json.Unmarshal(inner, &sub); err != nil {
+    		log.Printf("Error while parsing internal JSON: %s", err)
+    		log.Printf("Invalid JSON: %s", inner)
+    		err = nil
+    		continue
+    	}
+    	result = append(result, sub)
+    }
 
-	return
+    return
 }
 
 // SubscribeStream subscribe to stream by user ID
 func (session Session) SubscribeStream(userID string, lease int) (err error) {
-	type HubRequest struct {
-		Callback string `json:"hub.callback"`
-		Mode     string `json:"hub.mode"`
-		Topic    string `json:"hub.topic"`
-		Lease    int    `json:"hub.lease_seconds"`
-	}
-	body := HubRequest{}
-	body.Callback = session.config.CallbackServer + session.config.Port
-	body.Mode = "subscribe"
-	body.Topic = subscribeStreamTopic(userID)
-	body.Lease = lease
+    type HubRequest struct {
+    	Callback string `json:"hub.callback"`
+    	Mode     string `json:"hub.mode"`
+    	Topic    string `json:"hub.topic"`
+    	Lease    int    `json:"hub.lease_seconds"`
+    }
+    body := HubRequest{}
+    body.Callback = session.config.CallbackServer + session.config.Port
+    body.Mode = "subscribe"
+    body.Topic = subscribeStreamTopic(userID)
+    body.Lease = lease
 
-	bodyJSON, err := json.Marshal(body)
-	if err != nil {
-		log.Printf("Couldn't marshal the request JSON")
-		return
-	}
+    bodyJSON, err := json.Marshal(body)
+    if err != nil {
+    	log.Printf("Couldn't marshal the request JSON")
+    	return
+    }
 
-	req, err := postWebhookSubscription(bodyJSON)
-	if err != nil {
-		return
-	}
+    req, err := postWebhookSubscription(bodyJSON)
+    if err != nil {
+    	return
+    }
 
-	session.addTokenAuth(req)
-	req.Header.Set("Content-Type", "application/json")
-	res, err := session.client.Do(req)
-	if err != nil {
-		log.Printf("Error while executing SubscribeStream request: %s", err)
-		return
-	}
+    session.addTokenAuth(req)
+    req.Header.Set("Content-Type", "application/json")
+    res, err := session.client.Do(req)
+    if err != nil {
+    	log.Printf("Error while executing SubscribeStream request: %s", err)
+    	return
+    }
 
-	bodyRes, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		log.Printf("Error while reading SubscribeStream request's body: %s", err)
-		log.Printf("Body: %s", bodyRes)
-		return
-	}
+    bodyRes, err := ioutil.ReadAll(res.Body)
+    if err != nil {
+    	log.Printf("Error while reading SubscribeStream request's body: %s", err)
+    	log.Printf("Body: %s", bodyRes)
+    	return
+    }
 
-	return nil
+    return nil
 }
